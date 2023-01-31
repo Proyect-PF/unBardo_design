2c084d7486e1a5a9d4061d93cdd503c4e7ef65d6
Joaqoo555 <joaquincarrera2018@gmail.com>
2023-01-31 17:16:34 -0300

    Instalacion de dependencias, Inicializacion de Scripts, nodemon.json, transpilacion de codigo inicial

---
+++ api/index.ts
@@ -0,0 +1,12 @@
+// import express from "express";
+// const app = express();
+// const port = process.env.PORT || 3010;
+// import db from "./src/models/index";
+
+// // Syncing all the models at once.
+// db.sequelize.sync({force:false}).then(()=> {
+//     app.listen(port, ()=>{
+//         console.log(`App listening on port ${port}`)
+//     })
+// })
+

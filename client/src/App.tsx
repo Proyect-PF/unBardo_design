fc0f0ec47910e3491095e373c3c28dbaaa29e61d
Agustin Lago <agustin.lago@mi.unc.edu.ar>
2023-02-01 19:24:13 -0300

    Maquetado del HomePage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import Footer from "./layouts/Footer/Footer";
+import Navbar from "./layouts/Navbar/Navbar";
+// import Footer from "./layouts/Footer/Footer";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-        <Footer />
+      <Navbar/>
+        <Home />
+        {/* <Footer /> */}
     </div>
   );
 }

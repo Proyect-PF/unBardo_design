324469e9307abb1f3402b3bc42e30fdcd585d413
Diego Maidana <111674951+diegosdevep@users.noreply.github.com>
2023-02-01 01:29:22 +0100

    client folders

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Footer from './layouts/Footer/Footer';
+
+function App() {
+  return (
+    <div className='App'>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;

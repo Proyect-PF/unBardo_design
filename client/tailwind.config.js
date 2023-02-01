a738a10a7f39caf1f4a1dfe08d792b4ab79a5215
Agustin Lago <agustin.lago@mi.unc.edu.ar>
2023-02-01 12:51:31 -0300

    Setteo de colores globales tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      primary: "#FFFFFF",
+      secondary: "#000000",
+      details: "#F2FF58",
+      // ...
+    },
     extend: {},
   },
   plugins: [],

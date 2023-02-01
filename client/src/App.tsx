f08b92e7d4a956521e60bcacf2b457a1553070cc
kikeai <lg5967133@gmail.com>
2023-02-01 16:43:38 -0400

    sacar navbar de app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-import Index from "./layouts/";
 import Footer from "./layouts/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-
-        <Navbar />
         <Footer />
     </div>
   );

93818f9c099b6dd279ef342d2beaaec5c5809a19
Agustin Lago <agustin.lago@mi.unc.edu.ar>
2023-02-02 12:10:57 -0300

    Cambio de imagen en producto cuando se hace hover

--- client/src/components/Cards/Product/Product.tsx
+++ client/src/components/Cards/Product/Product.tsx
@@ -1,9 +1,14 @@
 import imageF from "../../../assets/images/remeras/unbardo-07F.png";
-// import imageB from "../../../assets/images/remeras/unbardo-07B.png";
+import imageB from "../../../assets/images/remeras/unbardo-07B.png";
 
 const Product = () => {
   return (
     <div className="flex-col w-56 gap-4 m-auto my-4 text-center">
+      <img
+        className="absolute w-56 m-auto border-b-2 border-black opacity-0 hover:opacity-100"
+        alt="black tshirt"
+        src={imageB}
+      />
       <img
         className="w-56 m-auto border-b-2 border-black"
         alt="black tshirt"

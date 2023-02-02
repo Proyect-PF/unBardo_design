db1e9b45c3138faa0fe584653ff516db9d0d9abf
Agustin Lago <agustin.lago@mi.unc.edu.ar>
2023-02-02 12:26:51 -0300

    Routeo basico home/product/login/adminpanel/404

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,14 +3,23 @@ import React from "react";
 import Navbar from "./layouts/Navbar/Navbar";
 import Footer from "./layouts/Footer/Footer";
 import Home from "./Pages/Home/Home";
-
+import { BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router";
 
 function App() {
   return (
-    <div >
-      <Navbar/>
-        <Home />
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="*" element={<div>404</div>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<div>detalles de producto</div>} />
+          <Route path="/account/login" element={<div>Login</div>} />
+          <Route path="/account/panel" element={<div>Admin Panel</div>} />
+        </Routes>
         <Footer />
+      </BrowserRouter>
     </div>
   );
 }

e92b95a0fdf5ebba41ee48f6e3fbf47109100499
Agustin Lago <agustin.lago@mi.unc.edu.ar>
2023-02-01 22:35:29 -0300

    Display Mockup de tarjetas estaticas en el HOME

--- client/src/Pages/Home/Home.tsx
+++ client/src/Pages/Home/Home.tsx
@@ -2,31 +2,32 @@ import photo from "../../assets/images/homep.jpg";
 import ig1 from "../../assets/images/ig1.jpg";
 import ig2 from "../../assets/images/ig2.jpg";
 import ig3 from "../../assets/images/ig3.jpg";
+import Product from "../../components/Cards/Product/Product";
 
 const Home = () => {
   return (
     <>
-      <img className="object-none w-full h-48" src={photo} />
+      <img className="object-none w-full h-48" alt="homepage" src={photo} />
       <p className="p-5 font-mono font-bold text-center border-b-2">
         WELCOME TO THE JUNGLE
       </p>
-      <div className="grid items-center w-full grid-cols-2 text-center border-b-2">
-        <div>Product Card</div>
-        <div>Product Card</div>
-        <div>Product Card</div>
-        <div>Product Card</div>
-        <div>Product Card</div>
-        <div>Product Card</div>
+      <div className="grid justify-center grid-cols-2 border-b-2">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
       </div>
 
-      <div className="flex flex-col items-center justify-center w-full h-48 gap-4 my-5 font-mono border-b-2">
+      <div className="flex flex-col items-center justify-center w-full h-48 gap-4 my-5 font-mono ">
         <p className="text-xs font-semibold leading-none text-center">
           ETIQUETANOS EN INSTAGRAM @UNBARDO
         </p>
         <div className="inline-flex items-center justify-center w-full pb-5 space-x-4 ">
-          <img className="w-24 h-full" src={ig1} />
-          <img className="w-24 h-full" src={ig3} />
-          <img className="w-24 h-full" src={ig2} />
+          <img className="w-24 h-full" alt="instagram post" src={ig1} />
+          <img className="w-24 h-full" alt="instagram post" src={ig3} />
+          <img className="w-24 h-full" alt="instagram post" src={ig2} />
         </div>
       </div>
     </>

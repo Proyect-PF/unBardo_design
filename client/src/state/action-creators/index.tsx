5e1ff45f2df591642ec65ff648ee976d070da71e
Agustin Lago <agustin.lago@mi.unc.edu.ar>
2023-02-03 15:24:33 -0300

    axios integration on action

--- client/src/state/action-creators/index.tsx
+++ client/src/state/action-creators/index.tsx
@@ -7,25 +7,23 @@ import { ProductState } from "../reducers/productReducer";
 export const getAllProducts = () => {
   return (dispatch: Dispatch<Action>) => {
     let payload: ProductState["productList"] = [];
-    fetch("http://localhost:3700/products")
-      .then((response) => response.json())
-      .then((res) => {
-        res.forEach((e: any) => {
-          payload.push({
-            id: e.id,
-            name: e.name,
-            description: e.description,
-            size: e.size,
-            price: e.price,
-            image: "",
-            show_in_shop: "",
-          });
-        });
-        dispatch({
-          type: ActionType.GET_ALL_PRODUCTS,
-          payload,
+    axios.get("http://localhost:3700/products").then((res) => {
+      res.data.forEach((e: any) => {
+        payload.push({
+          id: e.id,
+          name: e.name,
+          description: e.description,
+          size: e.size,
+          price: e.price,
+          image: "",
+          show_in_shop: "",
         });
       });
+      dispatch({
+        type: ActionType.GET_ALL_PRODUCTS,
+        payload,
+      });
+    });
   };
 };
 

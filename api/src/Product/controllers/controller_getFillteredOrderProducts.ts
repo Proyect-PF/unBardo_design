a2c1060d99f4b4bb13e04423af8c6f239e9fd0d4
Joaqoo555 <joaquincarrera2018@gmail.com>
2023-02-05 18:51:14 -0300

    refactoring filter by color and order

--- api/src/Product/controllers/controller_getFillteredOrderProducts.ts
+++ api/src/Product/controllers/controller_getFillteredOrderProducts.ts
@@ -1,21 +1,75 @@
 import { Express, Request, Response } from "express";
 import db from "../../models";
+import { Op } from "sequelize";
 
-  const getFillteredOrderProducts = async (req: Request , res:Response): Promise<Object> => {
-    try {
-      const { byColor , byOrder } = req.query;
-      const filteredProducts= await db.Product.findAll({
-        where: {
-            ['color'] : byColor,
-        },
-        order: [['price', byOrder? byOrder :"ASC"],],
+const getFillteredOrderProducts = async (
+  req: Request,
+  res: Response
+): Promise<Object> => {
+  try {
+    let filteredProducts;
+    const { byColor, byOrder } = req.query;
+    if (
+      byColor === "undefined" ||
+      !byColor ||
+      byOrder === "undefined" ||
+      byColor === "null" ||
+      byOrder === "null" ||
+      byColor === "all"
+    ) {
+      filteredProducts = await db.Product.findAll({
+         order: [['price', byOrder ? byOrder :"ASC"],],
         attributes: {
-          exclude: ['promotional_price', 'video', 'stock', , 'height', 'weight', 'width', 'length', 'SKU', 'barcode', 'createdAt', 'updatedAt', 'adminId', 'id_category']
-      }
+          exclude: [
+            "promotional_price",
+            "video",
+            "stock",
+            "height",
+            "weight",
+            "width",
+            "length",
+            "SKU",
+            "barcode",
+            "createdAt",
+            "updatedAt",
+            "adminId",
+            "id_category",
+          ],
+        },
       });
-      return res.status(200).json(filteredProducts);
-    } catch (error:any) {
-      return res.status(400).json(error.message);
+    } else {
+      filteredProducts = await db.Product.findAll({
+        where: {
+          color: { [Op.iLike]: byColor },
+        },
+        order: [["price", byOrder ? byOrder : "ASC"]],
+        attributes: {
+          exclude: [
+            "promotional_price",
+            "video",
+            "stock",
+            "height",
+            "weight",
+            "width",
+            "length",
+            "SKU",
+            "barcode",
+            "createdAt",
+            "updatedAt",
+            "adminId",
+            "id_category",
+          ],
+        },
+      });
     }
-  };
-  export default getFillteredOrderProducts;
+
+    if (!filteredProducts.length)
+      throw new Error(
+        "No se encontraron productos en la base de datos cargados"
+      );
+    return res.status(200).json(filteredProducts);
+  } catch (error: any) {
+    return res.status(400).json(error.message);
+  }
+};
+export default getFillteredOrderProducts;

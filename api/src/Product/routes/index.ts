f28427c81848dfc2ba1c0f3494a058f93ef0ffaf
Francisco <Franjabube@gmail.com>
2023-02-05 20:12:56 -0300

    Nada loco

--- api/src/Product/routes/index.ts
+++ api/src/Product/routes/index.ts
@@ -1,47 +1,53 @@
+// IMPORTS
 import { Router } from 'express';
-import controller_getFillteredOrderProducts from "../controllers/controller_getFillteredOrderProducts";
-import deleteImage from '../controllers/deleteImage';
+const productRoutes = Router();
+// PRODUCT FUNCTIONS
 import deleteProduct from '../controllers/deleteProduct';
-import filterByCategories from '../controllers/filterByCategories.controller';
-import filterByColors from '../controllers/filterByColors';
-import getImages from '../controllers/getImages';
 import getProduct from '../controllers/getProduct';
-import getVariants from "../controllers/getVariants";
-import orderByPrice from '../controllers/orderByPrice';
-import postImage from '../controllers/postImage';
 import postProduct from '../controllers/postProduct';
 import putProduct from '../controllers/putProductr';
+// PRODUCTS LIST FUNCTIONS
+import controller_getFillteredOrderProducts from "../controllers/controller_getFillteredOrderProducts";
 import searchProduct from '../controllers/searchProduct';
+
+
+
+
+import deleteImage from '../controllers/deleteImage';
+import getImages from '../controllers/getImages';
+import getVariants from "../controllers/getVariants";
+import postImage from '../controllers/postImage';
 import updateImage from '../controllers/updateImage';
 import validateImageId from '../middleware/validateImageId';
-const productRoutes = Router();
+
+
+
+
+
+
+// PRDUCT CRUD
+productRoutes.get("/", getProduct);
+productRoutes.delete('/', deleteProduct);
+productRoutes.put("/",putProduct);
+productRoutes.post("/", postProduct);
+// PRODUCTS LIST OPERATIONS
+productRoutes.get("/filtered/?", controller_getFillteredOrderProducts);
+productRoutes.get("/search/:search", searchProduct)
+
+
+
+
 
 
 productRoutes.delete("/images/:id", validateImageId, deleteImage);
-productRoutes.delete('/', deleteProduct);
-
-productRoutes.put("/",putProduct);
+productRoutes.post("/images", postImage);
 productRoutes.put("/images/:id", updateImage);
-
-
-
-productRoutes.get("/filtered/?", controller_getFillteredOrderProducts);
 productRoutes.get("/images/:id", getImages);
-productRoutes.get("/search/:search", searchProduct)
+
 productRoutes.get("/variants/:id", getVariants);
-
-
-
- productRoutes.get("/", getProduct);
 //productRoutes.get("/price/:price", orderByPrice);
 //productRoutes.get("/filterByCategories/:categoriId", filterByCategories);
 
-
-
-
-
-productRoutes.post("/", postProduct);
-productRoutes.post("/images", postImage);
 
 
 

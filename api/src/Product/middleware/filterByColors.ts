ec07b479aff40a6297f3b91d449ed65a973d42f4
Francisco <Franjabube@gmail.com>
2023-02-05 12:01:58 -0300

    Color repared

--- api/src/Product/middleware/filterByColors.ts
+++ api/src/Product/middleware/filterByColors.ts
@@ -1,11 +1,16 @@
 import db from "../../models";
 
-const filterByColors = async (colour: string): Promise<any> => {
+const filterByColors = async (color: string): Promise<any> => {
     try {
-        const filteredProduct = await db.Product.findAll({ where: { colour } });
+        const filteredProduct = await db.Product.findAll({ 
+            where: {
+                color : `${color}`
+            } });
+            console.log(filteredProduct)
         return filteredProduct;
+
     } catch (error: unknown) {
-        throw new Error("No es posible obtener el producto");
+        throw new Error(`No es posible obtener el producto. Ser recibio: ${color}`);
     }
 }
 

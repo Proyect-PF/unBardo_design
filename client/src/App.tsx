bb90cb985ccebadf71ec0e7145379a221dfc7940
Francisco <Franjabube@gmail.com>
2023-02-05 11:24:09 -0300

    QuickFix NewProductForm

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 
 import { Route, Routes } from "react-router";
 import { BrowserRouter } from "react-router-dom";
-import NewProduct from "./components/NewProduct/NewProduct";
+import NewProductForm from "./components/NewProduct/NewProductForm";
 import Footer from "./layouts/Footer/Footer";
 import Navbar from "./layouts/Navbar/Navbar";
 import Sidebar from "./layouts/Sidebar/Sidebar";
+import Checkout from "./Pages/Checkout/Checkout";
 import Details from "./Pages/Details/Details";
 import Home from "./Pages/Home/Home";
 import LogIn from "./Pages/LogIn/LogIn";
-import Checkout from "./Pages/Checkout/Checkout";
 
 import Searchbar from "./layouts/Searchbar/Searchbar";
 
@@ -44,7 +44,7 @@ function App() {
           <Route path="/account/login" element={<LogIn />} />
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/account/panel" element={<div>Admin Panel</div>} />
-          <Route path="/panel/newproduct" element={<NewProduct />} />
+          <Route path="/panel/newproduct" element={<NewProductForm />} />
         </Routes>
         <Footer />
       </BrowserRouter>

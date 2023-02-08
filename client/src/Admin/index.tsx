0fca36a85af60c84da7b49d9602e33440b4224dc
Agustin Lago <agustin.lago@mi.unc.edu.ar>
2023-02-07 22:43:52 -0300

    default Panel Admin config v2

--- client/src/Admin/index.tsx
+++ client/src/Admin/index.tsx
@@ -1,10 +1,10 @@
 import { Admin, ListGuesser, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 
-const dataProdiver = jsonServerProvider("http://localhost:3700/");
+const dataProdiver = jsonServerProvider("http://localhost:3700");
 
 const AdminPanel = () => (
-  <Admin dataProvider={dataProdiver}>
+  <Admin basename="/admin" dataProvider={dataProdiver}>
     <Resource name="products" list={ListGuesser} />
   </Admin>
 );

231f04a9e0c3f2bdb223e339962a9aa56d96beef
Matías Carrizo <matiascarrizo.msjz@gmail.com>
2023-02-08 11:56:27 -0300

    se agrego mercado pago a orders

--- api/src/Orders/routes/index.ts
+++ api/src/Orders/routes/index.ts
@@ -1,9 +1,22 @@
-// routes/orders.ts
 import { Router } from "express";
+const ordersRoutes = Router();
+
+import {
+    POST_GeneratePayment,
+    GET_FeedbackPayment    
+} from "../controllers/controllers_order";
+
+//MERCADOPAGO
+ordersRoutes.post("/payment/", POST_GeneratePayment);
+
+ordersRoutes.get("/feedback/", GET_FeedbackPayment);
+
+export default ordersRoutes;
+
+/*
 import * as ordersController from "../controllers/index";
 import * as authMiddleware from "../middlewares/index";
 
-const router = Router();
 
 // Get all orders
 router.get("/", authMiddleware.verifyToken, ordersController.getAllOrders);
@@ -21,4 +34,4 @@ router.get("/:id", authMiddleware.verifyToken, ordersController.getOrderById);
 // router.delete("/:id", authMiddleware.verifyToken, ordersController.deleteOrder);
 
 export default router;
-
+*/

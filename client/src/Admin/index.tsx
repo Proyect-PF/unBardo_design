0ed16f8ea432caffe4c204a09756895fed24a4d0
Agustin Lago <agustin.lago@mi.unc.edu.ar>
2023-02-07 22:48:17 -0300

    Custom List

--- client/src/Admin/index.tsx
+++ client/src/Admin/index.tsx
@@ -1,11 +1,12 @@
-import { Admin, ListGuesser, Resource } from "react-admin";
+import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
+import { ProductList } from "./Product/List";
 
 const dataProdiver = jsonServerProvider("http://localhost:3700");
 
 const AdminPanel = () => (
   <Admin basename="/admin" dataProvider={dataProdiver}>
-    <Resource name="products" list={ListGuesser} />
+    <Resource name="products" list={ProductList} />
   </Admin>
 );
 

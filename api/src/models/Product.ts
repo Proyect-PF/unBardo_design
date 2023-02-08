785b152167f2da5a8abda09a99499db05e31bce5
Juan Rodriguez <juan>
2023-02-08 15:38:32 +0100

    asociacion con CART para el carrito de compras en BACK

--- api/src/models/Product.ts
+++ api/src/models/Product.ts
@@ -21,10 +21,9 @@ interface PorductAttributes {
 }
 //TODO: show_in_shop se utiliza para el borrado lógico, toma valores true y false
 //TODO: stock es la cantidad disponible del producto, si el valor es cero debe mostrar OUT OF STOCK
-export default (sequelize:any, DataTypes:any) => {
+export default (sequelize: any, DataTypes: any) => {
   //class Product extends Model implements Model<PorductAttributes>{
-    class Product extends Model {
-
+  class Product extends Model {
     id!: number;
     name!: string;
     description!: Text;
@@ -40,113 +39,113 @@ export default (sequelize:any, DataTypes:any) => {
     length!: number;
     SKU!: string;
     barcode!: string;
-    id_category!:number;
-    image!:string;
+    id_category!: number;
+    image!: string;
 
+    static associate(models: any) {
+      // Product.hasMany(models.Image, {
+      //   foreignKey: 'productId',
+      //   as: 'images',
+      // })S
 
-      static associate(models: any) {
-        // Product.hasMany(models.Image, {
-        //   foreignKey: 'productId',
-        //   as: 'images',
-        // })
-        
-        Product.belongsTo(models.Category, {
-            foreignKey: 'id_category',
-        } )
-        Product.belongsToMany(models.Orders, {
-          through: models.OrderProducts,
-          foreignKey: 'id_product',
-          as: 'orders'
-        });
-        // Product.hasMany(models.ProductVariant, {
-        //   foreignKey: 'productId',
-        //   as: 'variants',
-        // });
+      models.Product.hasOne(models.Cart, { foreignKey: "id_cart" });
 
-      }
-
-
+      models.Product.belongsTo(models.Category, {
+        foreignKey: "id_category",
+      });
+      models.Product.belongsToMany(models.Orders, {
+        through: models.OrderProducts,
+        foreignKey: "id_product",
+        as: "orders",
+      });
+      // Product.hasMany(models.ProductVariant, {
+      //   foreignKey: 'productId',
+      //   as: 'variants',
+      // });
     }
-  Product.init({
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false
-    },
-    color: {
-      type: DataTypes.STRING,
-      // allowNull: false
-    },
-    image: {
-      type: DataTypes.STRING
-    },
-    name: {
-      type: DataTypes.STRING,
-      // allowNull: false
-    },
-    description: {
-      type: DataTypes.TEXT,
-      // allowNull: false
-    },
-    size: {
-      //type: Sequelize.ENUM("XS", "S", "M", "L", "XL"),
-      type: DataTypes.STRING,
-      // allowNull: false,
-      //defaultValue: "XS"
-    },
-    show_in_shop: {
-      //type: Sequelize.ENUM("si", "no"),
+  }
+  Product.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      color: {
+        type: DataTypes.STRING,
+        // allowNull: false
+      },
+      image: {
+        type: DataTypes.STRING,
+      },
+      name: {
+        type: DataTypes.STRING,
+        // allowNull: false
+      },
+      description: {
+        type: DataTypes.TEXT,
+        // allowNull: false
+      },
+      size: {
+        //type: Sequelize.ENUM("XS", "S", "M", "L", "XL"),
+        type: DataTypes.STRING,
+        // allowNull: false,
+        //defaultValue: "XS"
+      },
+      show_in_shop: {
+        //type: Sequelize.ENUM("si", "no"),
 
-      type: DataTypes.STRING,
-      // allowNull: false,
+        type: DataTypes.STRING,
+        // allowNull: false,
 
-      //defaultValue: "no"
+        //defaultValue: "no"
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        // allowNull: false
+      },
+      promotional_price: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      video: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      weight: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      width: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      height: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      length: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      SKU: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      barcode: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
     },
-    price: {
-      type: DataTypes.INTEGER,
-      // allowNull: false
-    },
-    promotional_price: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    video: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    weight: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    width: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    height: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    length: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    SKU: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    barcode: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-  }, {
-    sequelize,
-    modelName: 'Product',
-  });
+    {
+      sequelize,
+      modelName: "Product",
+    }
+  );
   return Product;
 };
-

b0c1bed73fd02f05a31cf00c6a2da49921927243
Agustin Lago <agustin.lago@mi.unc.edu.ar>
2023-02-08 22:57:38 -0300

    missing files added

--- api/src/Orders/routes/index.ts
+++ api/src/Orders/routes/index.ts
@@ -1,24 +1,27 @@
-// routes/orders.ts
 import { Router } from "express";
+const ordersRoutes = Router();
+
+import {
+    POST_GeneratePayment,
+    GET_FeedbackPayment    
+} from "../controllers/controlles_order";
+
+//MERCADOPAGO
+ordersRoutes.post("/payment/", POST_GeneratePayment);
+
+ordersRoutes.get("/feedback/", GET_FeedbackPayment);
+
+export default ordersRoutes;
+
+/*
 import * as ordersController from "../controllers/index";
 import * as authMiddleware from "../middlewares/index";
 
-const router = Router();
 
 // Get all orders
 router.get("/", authMiddleware.verifyToken, ordersController.getAllOrders);
-
-// Create a new order
-router.post("/", authMiddleware.verifyToken, ordersController.createOrder);
-
-// Get an order by ID
-router.get("/:id", authMiddleware.verifyToken, ordersController.getOrderById);
-
-// // Update an order by ID
-// router.put("/:id", authMiddleware.verifyToken, ordersController.updateOrder);
-//
-// // Delete an order by ID
+	@@ -34,4 +21,4 @@ router.get("/:id", authMiddleware.verifyToken, ordersController.getOrderById);
 // router.delete("/:id", authMiddleware.verifyToken, ordersController.deleteOrder);
 
 export default router;
-
+*/
---
+++ api/src/validations/routes.ts
@@ -0,0 +1,10 @@
+// IMPORTS
+import { Router } from "express";
+const validationsRoutes = Router();
+
+import validateOrder from "./validateOrder";
+
+
+validationsRoutes.post("/stock", validateOrder);
+
+export default validateOrder;

b503e16f1b1a65ecc4c0225e42ad9a9aa9fbef32
Agustin Lago <agustin.lago@mi.unc.edu.ar>
2023-02-10 18:56:06 -0300

    minimal change

--- client/src/AdminPanel/AdminRedux/store.tsx
+++ client/src/AdminPanel/AdminRedux/store.tsx
@@ -1,5 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
 import adminReducer from "./reducer";
 import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-export const store = createStore(adminReducer, {}, applyMiddleware(thunk));
+export const store = createStore(
+  adminReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);

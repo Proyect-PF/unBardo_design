f41d3a89ed7ba7d3421bb8ffbded49e1c613c387
Joaqoo555 <joaquincarrera2018@gmail.com>
2023-02-11 08:14:16 -0300

    delete archives back

--- api/src/Order/controllers/deleteOrder.ts
+++
@@ -1,7 +0,0 @@
-import express, { Response, Request, Router } from "express";
-
-const deleteOrder = (request: Request, response: Response) => {
-  response.send("delete order");
-};
-
-export default deleteOrder;

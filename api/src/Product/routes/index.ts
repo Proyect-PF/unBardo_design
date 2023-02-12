5bc60a8f097c23efc83bdfe36b2703d0c811d4e8
Alejandro Asor Corrales Gomez <84219142+AlejandroAsor@users.noreply.github.com>
2023-02-12 00:39:29 -0500

    Implement DELETE_DeleteAllProducts to delete all products

--- api/src/Product/routes/index.ts
+++ api/src/Product/routes/index.ts
@@ -1,9 +1,10 @@
 // IMPORTS
-import { Router } from "express";
+import {Router} from "express";
+
 const productRoutes = Router();
 
 import {
-    DELETE_DeleteProduct, GET_AllProducts, GET_ProductById, GET_SearchByName, POST_NewProduct,
+    DELETE_DeleteProduct, DELETE_DeleteAllProducts, GET_AllProducts, GET_ProductById, GET_SearchByName, POST_NewProduct,
     UPDATE_UpdateProduct
 } from "../controllers/controllers_product";
 
@@ -13,10 +14,9 @@ import getVariants from "../controllers/getVariants";
 import postImage from "../controllers/postImage";
 import updateImage from "../controllers/updateImage";
 import validateImageId from "../middleware/validateImageId";
-import { verifyTokenIsAdmin } from "../../helpers/verifyTokenIsAdmin";
+import {verifyTokenIsAdmin} from "../../helpers/verifyTokenIsAdmin";
 
 //productRoutes.get("/:id", GET_AllProducts);
-
 
 
 //productRoutes.get("/id/:id", GET_ProductById);
@@ -29,22 +29,16 @@ productRoutes.get("/", GET_AllProducts);
 productRoutes.get("/:id", GET_ProductById);
 productRoutes.put("/", UPDATE_UpdateProduct);
 
-productRoutes.delete("/delete/:id", verifyTokenIsAdmin,DELETE_DeleteProduct);
+productRoutes.delete("/delete/:id", verifyTokenIsAdmin, DELETE_DeleteProduct);
+productRoutes.delete("/deleteAll", verifyTokenIsAdmin, DELETE_DeleteAllProducts);
 
 
 productRoutes.get("/search/:name", GET_SearchByName);
 
-productRoutes.delete("/:id", verifyTokenIsAdmin,DELETE_DeleteProduct);
+productRoutes.delete("/:id", verifyTokenIsAdmin, DELETE_DeleteProduct);
 
 
-productRoutes.post("/",verifyTokenIsAdmin, POST_NewProduct);
-
-
-
-
-
-
-
+productRoutes.post("/", verifyTokenIsAdmin, POST_NewProduct);
 
 
 //   ALEJANDRO EN REVISION POR JOAQUIN CARRERA

adde6cbe5df9832ee265d787daf71b5b2e497f73
Diego Maidana <111674951+diegosdevep@users.noreply.github.com>
2023-02-12 16:53:56 -0300

    vaciar carrito

--- client/src/Pages/Details/Details.tsx
+++ client/src/Pages/Details/Details.tsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { useLocation, useNavigate } from "react-router-dom";
-import { bindActionCreators } from "redux";
-import imageB from "../../assets/images/remeras/unbardo-07B.png";
-import imageF from "../../assets/images/remeras/unbardo-07F.png";
-import Button from "../../components/Buttons/Button/Button";
-import AmountInput from "../../components/Inputs/Amount/AmountInput";
-import SizeSelector from "../../components/Inputs/SizeSelector/SizeSelector";
-import { actionCreators } from "../../state";
-import { State } from "../../state/reducers";
+import { useLocation, useNavigate } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+import imageB from '../../assets/images/remeras/unbardo-07B.png';
+import imageF from '../../assets/images/remeras/unbardo-07F.png';
+import Button from '../../components/Buttons/Button/Button';
+import AmountInput from '../../components/Inputs/Amount/AmountInput';
+import SizeSelector from '../../components/Inputs/SizeSelector/SizeSelector';
+import { actionCreators } from '../../state';
+import { State } from '../../state/reducers';
 
-import { getItem } from "../../utils/localStorage";
+import { getItem } from '../../utils/localStorage';
 
 const Details = (): JSX.Element => {
   const location = useLocation();
@@ -26,10 +26,10 @@ const Details = (): JSX.Element => {
   const [loading, setLoading] = useState(true);
   //AL: size / amount state retrieve the selection for future add to cart implementation
   const [ammount, setAmmount] = useState(1);
-  const [size, setSize] = useState("");
+  const [size, setSize] = useState('');
 
   //AL:retrieve the id from the url & transform to number to match type
-  const idS = location.pathname.split("/")[2];
+  const idS = location.pathname.split('/')[2];
   const id = Number(idS);
 
   //AL:Same loading implementation as HOME page
@@ -42,15 +42,15 @@ const Details = (): JSX.Element => {
   //  but functional for now.
   useEffect(() => {
     setLoading(true);
-    !productDetails.show_in_shop && navigate("/");
-    if (productDetails.name !== "") setLoading(false);
+    !productDetails.show_in_shop && navigate('/');
+    if (productDetails.name !== '') setLoading(false);
   }, [productDetails]);
 
   //AL: this function manages the add to cart functionality, needs to be implemented
   const handleCart = (e: any) => {
     e.preventDefault();
     const payload = {
-      id: productDetails.id + "-" + size,
+      id: productDetails.id + '-' + size,
       name: productDetails.name,
       size: size,
       price: productDetails.price,
@@ -58,7 +58,7 @@ const Details = (): JSX.Element => {
       imgF: productDetails.image,
     };
     addCheckout(payload);
-    setSize("");
+    setSize('');
     setAmmount(1);
   };
 
@@ -66,11 +66,11 @@ const Details = (): JSX.Element => {
     productDetails.S + productDetails.L + productDetails.M + productDetails.XL;
 
   const stockSize = (size: string) => {
-    let checkoutList = getItem("shoppingBag");
-    if (size === "S") {
+    let checkoutList = getItem('shoppingBag');
+    if (size === 'S') {
       if (checkoutList?.length > 0) {
         let findCard = checkoutList.find(
-          (x: any) => x.id === productDetails.id + "-S"
+          (x: any) => x.id === productDetails.id + '-S'
         );
         if (findCard) {
           return productDetails.S - findCard.ammount;
@@ -78,10 +78,10 @@ const Details = (): JSX.Element => {
       }
       return productDetails.S;
     }
-    if (size === "L") {
+    if (size === 'L') {
       if (checkoutList?.length > 0) {
         let findCard = checkoutList.find(
-          (x: any) => x.id === productDetails.id + "-L"
+          (x: any) => x.id === productDetails.id + '-L'
         );
         if (findCard) {
           return productDetails.L - findCard.ammount;
@@ -89,10 +89,10 @@ const Details = (): JSX.Element => {
       }
       return productDetails.L;
     }
-    if (size === "M") {
+    if (size === 'M') {
       if (checkoutList?.length > 0) {
         let findCard = checkoutList.find(
-          (x: any) => x.id === productDetails.id + "-M"
+          (x: any) => x.id === productDetails.id + '-M'
         );
         if (findCard) {
           return productDetails.M - findCard.ammount;
@@ -100,10 +100,10 @@ const Details = (): JSX.Element => {
       }
       return productDetails.M;
     }
-    if (size === "XL") {
+    if (size === 'XL') {
       if (checkoutList?.length > 0) {
         let findCard = checkoutList.find(
-          (x: any) => x.id === productDetails.id + "-XL"
+          (x: any) => x.id === productDetails.id + '-XL'
         );
         if (findCard) {
           return productDetails.XL - findCard.ammount;
@@ -119,35 +119,35 @@ const Details = (): JSX.Element => {
     <div>
       <div
         className={`my-8 flex justify-center ${
-          !loading ? "hidden" : "visible"
+          !loading ? 'hidden' : 'visible'
         }`}
       >
-        <div className="border-8 border-black border-solid rounded-full w-44 h-44 border-t-transparent animate-spin"></div>
+        <div className='border-8 border-black border-solid rounded-full w-44 h-44 border-t-transparent animate-spin'></div>
       </div>
       <div
         className={`flex flex-col md:flex-row md:gap-5 ${
-          loading ? "hidden" : "visible"
+          loading ? 'hidden' : 'visible'
         }`}
       >
-        <img className="" alt="black tshirt" src={productDetails.image} />
+        <img className='' alt='black tshirt' src={productDetails.image} />
 
-        <div className="w-4/5 mx-auto md:flex md:flex-col md:justify-between md:mt-8">
+        <div className='w-4/5 mx-auto md:flex md:flex-col md:justify-between md:mt-8'>
           <div>
-            <p className="mt-4 text-4xl font-bold ">{productDetails.name}</p>
+            <p className='mt-4 text-4xl font-bold '>{productDetails.name}</p>
 
             {stock > 0 ? (
-              <p className="my-2 text-3xl font-bold ">{`$ ${productDetails.price}`}</p>
+              <p className='my-2 text-3xl font-bold '>{`$ ${productDetails.price}`}</p>
             ) : (
-              <div className="flex flex-row gap-4">
-                <p className="my-2 text-3xl italic font-bold text-gray-500 line-through">{`$ ${productDetails.price}`}</p>
-                <p className="my-2 text-xl italic font-bold ">Out of Stock</p>
+              <div className='flex flex-row gap-4'>
+                <p className='my-2 text-3xl italic font-bold text-gray-500 line-through'>{`$ ${productDetails.price}`}</p>
+                <p className='my-2 text-xl italic font-bold '>Out of Stock</p>
               </div>
             )}
 
-            <p className="my-2 text-lg italic font-medium font-poppins">{`${productDetails.description}`}</p>
+            <p className='my-2 text-lg italic font-medium font-poppins'>{`${productDetails.description}`}</p>
           </div>
           <div>
-            <div className="flex justify-around my-8 text-lg text-center">
+            <div className='flex justify-around my-8 text-lg text-center'>
               <SizeSelector
                 detailId={productDetails.id}
                 selected={size}
@@ -167,14 +167,14 @@ const Details = (): JSX.Element => {
               />
             </div>
 
-            <div className="flex flex-col">
+            <div className='flex flex-col'>
               <Button
-                className={"justify-center"}
-                type="button"
-                text="Añadir al carrito"
+                className={'justify-center'}
+                type='button'
+                text='Añadir al carrito'
                 onClick={handleCart}
-                name="Carrito"
-                disabled={size === ""}
+                name='Carrito'
+                disabled={size === ''}
               />
             </div>
           </div>

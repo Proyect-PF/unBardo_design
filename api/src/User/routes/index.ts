4cc1b02094d8d2a4ead95af232eb8ea1d09d2c4f
Alejandro Asor Corrales Gomez <84219142+AlejandroAsor@users.noreply.github.com>
2023-02-12 00:25:42 -0500

    Add implementation for DELETE_AllUsers to delete all users and their associated orders.

--- api/src/User/routes/index.ts
+++ api/src/User/routes/index.ts
@@ -1,22 +1,24 @@
-import { Router } from "express";
+import {Router} from "express";
 import {
-  DELETE_UserById,
-  GET_User,
-  POST_User,
-  UPDATE_User,
+    DELETE_UserById,
+    DELETE_AllUsers,
+    GET_User,
+    POST_User,
+    UPDATE_User,
 } from "../controllers/controllers_user";
-import { verifyTokenIsAdmin } from "../../helpers/verifyTokenIsAdmin";
+import {verifyTokenIsAdmin} from "../../helpers/verifyTokenIsAdmin";
 
 
 const userRoutes = Router();
 
-
 userRoutes.delete("/:id", DELETE_UserById);
+
+userRoutes.delete("/", DELETE_AllUsers);
 
 userRoutes.put("/:id", UPDATE_User);
 
 userRoutes.get("/", GET_User);
 
-userRoutes.post("/",POST_User);
+userRoutes.post("/", POST_User);
 
 export default userRoutes;

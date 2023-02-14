32bb40005373ae5ba03de1881405487543ef5c76
Agustin Lago <agustin.lago@mi.unc.edu.ar>
2023-02-14 10:39:03 -0300

    dependecies + emaillist export WIP

--- client/src/AdminPanel/Email/index.tsx
+++ client/src/AdminPanel/Email/index.tsx
@@ -1,13 +1,18 @@
 import Button from "../../components/Buttons/Button/Button";
 import Input from "../../components/Inputs/Input";
+import { baseURL, PORT } from "../../utils/url&port";
+import axios from "axios";
+import { CSVLink } from "react-csv";
+import React from "react";
 
 const EmailList = () => {
-  const headers = [
-    { label: "Nombre", key: "fullname" },
-    { label: "Email", key: "email" },
-  ];
+  let data: string = "email1";
+  let csvLinkEl: any = React.createRef();
 
-  const handleExport = async () => {};
+  const handleExport = async () => {
+    // data = await axios.get(`${baseURL}:${PORT}/....`);
+    csvLinkEl.current.link.click();
+  };
 
   return (
     <div className="flex flex-col gap-8 m-10">
@@ -25,6 +30,11 @@ const EmailList = () => {
             disabled={false}
             type="button"
             className="justify center"
+          />
+          <CSVLink
+            filename="Newsletter_User_List.csv"
+            data={data}
+            ref={csvLinkEl}
           />
         </div>
       </div>

a1a0216ae293e5ef49da504bfcc7d9049f55133e
Agustin Lago <agustin.lago@mi.unc.edu.ar>
2023-02-17 20:26:18 -0300

    unsuscribe page linked with backend route

--- client/src/state/action-creators/index.tsx
+++ client/src/state/action-creators/index.tsx
@@ -299,13 +299,10 @@ export const getOrderDetails = (
 ) => {
   return async (dispatch: Dispatch<ActionOrderCheckout>) => {
     try {
-      const response = await axios.post(
-        "http://localhost:3700/orders/feedback",
-        {
-          payment_id,
-          external_reference,
-        }
-      );
+      const response = await axios.post(`${baseURL}:${PORT}/orders/feedback`, {
+        payment_id,
+        external_reference,
+      });
       dispatch(getOrderDetailsSuccess(response.data));
       // console.log(response.data);
     } catch (error) {
@@ -316,115 +313,122 @@ export const getOrderDetails = (
 
 export const getFavorites = (id: number) => {
   return (dispatch: Dispatch<ActionFavorites>) => {
-    axios.get(`${baseURL}:${PORT}/favorites/${id}`)
-    .then((res) => {
-      const payload = res.data;
-      dispatch({
-        type: ActionType.GET_FAVORITES,
-        payload
+    axios
+      .get(`${baseURL}:${PORT}/favorites/${id}`)
+      .then((res) => {
+        const payload = res.data;
+        dispatch({
+          type: ActionType.GET_FAVORITES,
+          payload,
+        });
       })
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-  }
-}
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+};
 
 export const setFavorite = (payload: SetFavoritePayload, getFavorites: any) => {
   return (dispatch: Dispatch<ActionFavorites>) => {
-    axios.post(`${baseURL}:${PORT}/favorites`, payload)
-    .then((res) => {
-      const Toast = Swal.mixin({
-        toast: true,
-        position: "bottom",
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.addEventListener("mouseenter", Swal.stopTimer);
-          toast.addEventListener("mouseleave", Swal.resumeTimer);
-        },
-      });
+    axios
+      .post(`${baseURL}:${PORT}/favorites`, payload)
+      .then((res) => {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: "bottom",
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener("mouseenter", Swal.stopTimer);
+            toast.addEventListener("mouseleave", Swal.resumeTimer);
+          },
+        });
 
-      Toast.fire({
-        icon: "success",
-        title:
-          "<p class='font-bold font-rift text-black'>Se agregó a tus favoritos</p>",
-      });
-      getFavorites(payload.id_user)
-    })
-    .catch((err) => {
-      const Toast = Swal.mixin({
-        toast: true,
-        position: "bottom",
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.addEventListener("mouseenter", Swal.stopTimer);
-          toast.addEventListener("mouseleave", Swal.resumeTimer);
-        },
-      });
+        Toast.fire({
+          icon: "success",
+          title:
+            "<p class='font-bold font-rift text-black'>Se agregó a tus favoritos</p>",
+        });
+        getFavorites(payload.id_user);
+      })
+      .catch((err) => {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: "bottom",
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener("mouseenter", Swal.stopTimer);
+            toast.addEventListener("mouseleave", Swal.resumeTimer);
+          },
+        });
 
-      Toast.fire({
-        icon: "error",
-        title:
-          "<p class='font-bold font-rift text-black'>No se pudo agregar a tus favoritos</p>",
-    })
-  })
-  }
-}
+        Toast.fire({
+          icon: "error",
+          title:
+            "<p class='font-bold font-rift text-black'>No se pudo agregar a tus favoritos</p>",
+        });
+      });
+  };
+};
 
-export const deleteFavorite = (payload: SetFavoritePayload, getFavorites: any) => {
+export const deleteFavorite = (
+  payload: SetFavoritePayload,
+  getFavorites: any
+) => {
   return (dispatch: Dispatch<ActionFavorites>) => {
-    axios.delete(`${baseURL}:${PORT}/favorites?id_user=${payload.id_user}&id_product=${payload.id_product}`,)
-    .then((res) => {
-      const Toast = Swal.mixin({
-        toast: true,
-        position: "bottom",
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.addEventListener("mouseenter", Swal.stopTimer);
-          toast.addEventListener("mouseleave", Swal.resumeTimer);
-        },
-      });
+    axios
+      .delete(
+        `${baseURL}:${PORT}/favorites?id_user=${payload.id_user}&id_product=${payload.id_product}`
+      )
+      .then((res) => {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: "bottom",
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener("mouseenter", Swal.stopTimer);
+            toast.addEventListener("mouseleave", Swal.resumeTimer);
+          },
+        });
 
-      Toast.fire({
-        icon: "success",
-        title:
-          "<p class='font-bold font-rift text-black'>Se eliminó de tus favoritos</p>",
-      });
-      getFavorites(payload.id_user)
-    })
-    .catch((err) => {
-      const Toast = Swal.mixin({
-        toast: true,
-        position: "bottom",
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.addEventListener("mouseenter", Swal.stopTimer);
-          toast.addEventListener("mouseleave", Swal.resumeTimer);
-        },
-      });
+        Toast.fire({
+          icon: "success",
+          title:
+            "<p class='font-bold font-rift text-black'>Se eliminó de tus favoritos</p>",
+        });
+        getFavorites(payload.id_user);
+      })
+      .catch((err) => {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: "bottom",
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener("mouseenter", Swal.stopTimer);
+            toast.addEventListener("mouseleave", Swal.resumeTimer);
+          },
+        });
 
-      Toast.fire({
-        icon: "error",
-        title:
-          "<p class='font-bold font-rift text-black'>No se pudo eliminar de tus favoritos</p>",
-    })
-  })
-  }
-}
+        Toast.fire({
+          icon: "error",
+          title:
+            "<p class='font-bold font-rift text-black'>No se pudo eliminar de tus favoritos</p>",
+        });
+      });
+  };
+};
 
 export const logOutFavorites = () => {
   return (dispatch: Dispatch<ActionFavorites>) => {
-      dispatch({
-        type: ActionType.LOG_OUT_FAVORITES,
-    })
-  }
-}
-
+    dispatch({
+      type: ActionType.LOG_OUT_FAVORITES,
+    });
+  };
+};

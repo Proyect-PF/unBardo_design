66ae960c6f3f63471b687592c5139ec69549a138
Alejandro Asor Corrales Gomez <84219142+AlejandroAsor@users.noreply.github.com>
2023-02-18 23:17:38 -0500

    Update statistic.routes.ts

--- api/src/controllers/statistic.controller.ts
+++ api/src/controllers/statistic.controller.ts
@@ -1,74 +1,52 @@
-import { Request, Response } from "express";
-import { QueryTypes } from "sequelize";
-import moment from "moment";
-import db from "../database/database";
+import {Request, Response} from 'express';
+import moment from 'moment';
+import db from '../database/database';
+
+interface DailyStatistic {
+    timeUnit: string;
+    totalProductsSold: number;
+}
 
 export const getProductSalesStats = async (req: Request, res: Response) => {
     try {
-        const { timeUnit, status } = req.query;
-
-        let timeUnitFormat: any;
-        let groupByTimeUnit;
-        let dateRange;
-
-        switch (timeUnit) {
-            case "day":
-                timeUnitFormat = "YYYY-MM-DD";
-                groupByTimeUnit = "date_trunc('day', \"Orders\".\"createdAt\")";
-                dateRange = "";
-                break;
-            case "week":
-                timeUnitFormat = "YYYY [W]WW";
-                groupByTimeUnit = "to_char(\"Orders\".\"createdAt\" - INTERVAL '1 day', 'IYYY \"W\"IW')";
-                dateRange = `AND \"Orders\".\"createdAt\" >= NOW() - INTERVAL '20 years'`;
-                break;
-
-            case "month":
-                timeUnitFormat = "YYYY-MM";
-                groupByTimeUnit = "to_char(\"Orders\".\"createdAt\", 'YYYY-MM')";
-                dateRange = `AND \"Orders\".\"createdAt\" >= NOW() - INTERVAL '20 years'`;
-                break;
-            case "year":
-                timeUnitFormat = "YYYY";
-                groupByTimeUnit = "DATE_TRUNC('year', \"Orders\".\"createdAt\")";
-                dateRange = `AND \"Orders\".\"createdAt\" >= NOW() - INTERVAL '20 years'`;
-                break;
-
-            default:
-                return res.status(400).json({
-                    message: `Invalid time unit provided: ${timeUnit}. Valid values are: day, week, month, year.`,
-                });
+        let {timeUnit, num}: any = req.query;
+        if (!timeUnit) {
+            return res.status(400).json({message: 'Missing period parameter'});
         }
+        if (!num && timeUnit === 'days') {
+            num = 7;
+        } else if (!num && timeUnit === 'weeks') {
+            num = 4;
+        } else if (!num && timeUnit === 'months') {
+            num = 12;
+        } else if (!num && timeUnit === 'years') {
+            num = 5;
+        }
+        const range = moment().subtract(num, timeUnit).format('YYYY-MM-DD');
 
-        const stats = await db.sequelize.query(
-            `SELECT ${groupByTimeUnit} AS "timeUnit",
-    sum(("OrderProducts"."sizes" ->> 'S')::integer + 
-        ("OrderProducts"."sizes" ->> 'M')::integer + 
-        ("OrderProducts"."sizes" ->> 'L')::integer + 
-        ("OrderProducts"."sizes" ->> 'XL')::integer) AS "totalProductsSold"
-  FROM "Orders"
-  INNER JOIN "OrderProducts" ON "Orders"."id" = "OrderProducts"."id_order"
-  WHERE "Orders"."status" = '${status}' ${dateRange}
-  GROUP BY "timeUnit"
-  ORDER BY "timeUnit" ASC`,
-            { type: QueryTypes.SELECT }
-        );
+        const query = `
+      SELECT DATE_TRUNC('${timeUnit}', orders."updatedAt") AS "timeUnit",
+        SUM(COALESCE((op.sizes->>'XL')::int, 0) 
+          + COALESCE((op.sizes->>'S')::int, 0) 
+          + COALESCE((op.sizes->>'M')::int, 0) 
+          + COALESCE((op.sizes->>'L')::int, 0)) AS "totalProductsSold"
+      FROM public."OrderProducts" AS op
+      JOIN public."Orders" AS orders ON op.id_order = orders.id
+      WHERE orders."updatedAt" >= '${range}'
+      GROUP BY DATE_TRUNC('${timeUnit}', orders."updatedAt")
+      ORDER BY DATE_TRUNC('${timeUnit}', orders."updatedAt") ASC
+    `;
+
+        const results = await db.sequelize.query(query, {
+            type: db.sequelize.QueryTypes.SELECT,
+        }) as DailyStatistic[];
 
 
 
-
-
-        const data = stats.map((stat: { timeUnit: string; totalProductsSold: string }) => {
-            return {
-                timeUnit: moment(stat.timeUnit, 'YYYY-MM-DD').format(timeUnitFormat),
-                totalProductsSold: parseInt(stat.totalProductsSold),
-            };
-        });
-
-
-        return res.status(200).json(data);
+        return res.status(200).json(results);
     } catch (error) {
         console.error(error);
-        return res.status(500).json({ message: "Internal server error" });
+        return res.status(500).json({message: 'Internal server error'});
     }
 };
+
--- api/src/routes/statistic.routes.ts
+++ api/src/routes/statistic.routes.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 const statisticRoutes = Router();
 import {getProductSalesStats} from "../controllers/statistic.controller";
 
-statisticRoutes.get("/product-sales", getProductSalesStats);
+statisticRoutes.get("/product-sales/", getProductSalesStats);
 
 export default statisticRoutes;

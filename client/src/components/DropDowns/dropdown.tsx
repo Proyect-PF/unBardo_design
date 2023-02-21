91a423826e0047a114c81f96bd4a2f759dcc1dd3
Agustin Lago <agustin.lago@mi.unc.edu.ar>
2023-02-20 23:16:26 -0300

    home page pagination integration

--- client/src/components/DropDowns/dropdown.tsx
+++ client/src/components/DropDowns/dropdown.tsx
@@ -1,20 +1,23 @@
 import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { bindActionCreators } from "redux";
 import arrow from "../../assets/svg/come-back.svg";
 import { actionCreators } from "../../state";
 import { fetch_products } from "../../state/action-creators";
+import { State } from "../../state/reducers";
 
 const Dropdown = (): JSX.Element => {
   const dispatch = useDispatch();
 
   const [show, setShow] = useState(false);
-
+  const { productCount } = useSelector((state: State) => state.products);
   const { fetch_products } = bindActionCreators(actionCreators, dispatch);
   const INITIAL_STATE = {
     byColor: "",
     byOrder: "ASC",
     byPromo: "",
+    page: 1,
+    perPage: 20,
   };
   const [Query, setQuery] = useState(INITIAL_STATE);
 
@@ -23,6 +26,7 @@ const Dropdown = (): JSX.Element => {
     setQuery({
       ...Query,
       [name]: value,
+      page: 1,
     });
   };
 
@@ -30,9 +34,20 @@ const Dropdown = (): JSX.Element => {
     show ? setShow(false) : setShow(true);
   };
 
+  const handlePag = (event: React.MouseEvent<HTMLButtonElement>): void => {
+    const button: HTMLButtonElement = event.currentTarget;
+    if (button.id === "-" && Query.page !== 1)
+      setQuery({ ...Query, page: Query.page - 1 });
+    if (
+      button.id === "+" &&
+      Math.ceil(productCount / Query.perPage) > Query.page
+    )
+      setQuery({ ...Query, page: Query.page + 1 });
+  };
+
   useEffect(() => {
     fetch_products(
-      `filter=${Query.byColor}&filter2=${Query.byPromo}&order=${Query.byOrder}&sort=price`
+      `filter=${Query.byColor}&filter2=${Query.byPromo}&order=${Query.byOrder}&sort=price&page=${Query.page}&perPage=${Query.perPage}`
     );
   }, [Query]);
 
@@ -75,7 +90,41 @@ const Dropdown = (): JSX.Element => {
         >
           <option value="ASC">{"Menor precio"}</option>
           <option value="DESC">{"Mayor precio"}</option>
-        </select>{" "}
+        </select>
+      </div>
+      <div
+        className={`flex justify-center gap-4 mx-4 mt-4 ${
+          show ? "visible" : "hidden"
+        }`}
+      >
+        <div className="flex flex-row gap-2">
+          <button
+            className="h-fit"
+            onClick={handlePag}
+            id="-"
+            name="-"
+          >{`<`}</button>
+          <p>{Query.page}</p>
+          <button
+            className="h-fit"
+            onClick={handlePag}
+            id="+"
+            name="+"
+          >{`>`}</button>
+        </div>
+        <select
+          className="h-fit"
+          id="perPage"
+          name="perPage"
+          value={Query.perPage}
+          onChange={handleChange}
+        >
+          <option value="10" selected>
+            10
+          </option>
+          <option value="15">15</option>
+          <option value="20">20</option>
+        </select>
       </div>
       <img
         src={arrow}

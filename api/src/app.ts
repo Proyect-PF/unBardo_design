44a84c14ddcedc10deb2d9b5eaaaba038bf122df
Joaquin Carrera <110292827+Joaqoo555@users.noreply.github.com>
2023-02-21 00:49:47 -0300

    Update app.ts
    res.header('Set-Cookie', 'G_AUTHUSER_H=; SameSite=None; Secure');
    
    ERROR DE CORS

--- api/src/app.ts
+++ api/src/app.ts
@@ -38,6 +38,7 @@ app.use(helmet())
    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
    res.header('Access-Control-Allow-Credentials', 'true');
    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+   res.header('Set-Cookie', 'G_AUTHUSER_H=; SameSite=None; Secure');
    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
    next();
  });

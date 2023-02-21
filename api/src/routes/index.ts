96a675ac00b23cc7f871e6acea38dcf493636e9c
Alejandro Asor Corrales Gomez <84219142+AlejandroAsor@users.noreply.github.com>
2023-02-20 22:24:36 -0500

    Add Visits Counter

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import db from '../database/database';
 
 // Importa los enrutadores del módulo 'routes'.
 import authRouter from './auth.routes';
@@ -11,8 +12,26 @@ import imageRoutes from "./image.routes";
 import shipmentsRoutes from "./shipment.routes"
 import statisticRoutes from "./statistic.routes";
 
+// Define una función para el middleware de estadísticas
+function trackStats(req:any, res:any, next:any) {
+    db.Statistics.create({
+        action_type: 'visit',
+        path: req.originalUrl
+    })
+        .then(() => {
+            next();
+        })
+        .catch((error:any) => {
+            console.error('Error creating statistic:', error);
+            next();
+        });
+}
+
 // Crea una instancia de un enrutador de Express.
 const index = Router();
+
+// Usa la función del middleware de estadísticas
+index.use(trackStats);
 
 // Define las rutas para cada enrutador.
 index.use('/auth', authRouter);

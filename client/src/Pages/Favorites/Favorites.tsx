c19ee25ae7c35384855158a7add000b18537eba0
Agustin Lago <agustin.lago@mi.unc.edu.ar>
2023-02-22 10:46:58 -0300

    styling logo + favs

--- client/src/Pages/Favorites/Favorites.tsx
+++ client/src/Pages/Favorites/Favorites.tsx
@@ -7,56 +7,57 @@ import { actionCreators } from "../../state";
 import { State } from "../../state/reducers";
 
 const Favorites = () => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const { getFavorites } = bindActionCreators(actionCreators, dispatch);
+  const { getFavorites } = bindActionCreators(actionCreators, dispatch);
 
-    const { productList } = useSelector(
-        (state: State) => state.products
-      );
+  const { productList } = useSelector((state: State) => state.products);
 
-    const { success, userId } = useSelector(
-        (state: State) => state.user
-      );
+  const { success, userId } = useSelector((state: State) => state.user);
 
-    const { products_id } = useSelector(
-        (state: State) => state.favorites
-      );
+  const { products_id } = useSelector((state: State) => state.favorites);
 
-    useEffect(() => {
-        if(!success || !productList.length) navigate("/");
-        getFavorites(userId);
-    }, [])
+  useEffect(() => {
+    if (!success || !productList.length) navigate("/");
+    getFavorites(userId);
+  }, []);
 
-    const productsFavorites = productList.filter((product) => products_id.includes(product.id))
+  const productsFavorites = productList.filter((product) =>
+    products_id.includes(product.id)
+  );
 
-    return (
-        <div>
-            <h3 className="font-rift text-4xl font-bold text-center">Productos Favoritos</h3>
-            {
-
-                productsFavorites.length? <div className="grid grid-cols-1 mx-auto w-fit md:grid-cols-2 lg:grid-cols-3 md:gap-x-12 xl:grid-cols-4 2xl:grid-cols-6">
-                    {
-                        productsFavorites.map((e) => {
-                            if (e.show_in_shop) {
-                            return (
-                                <Product
-                                image={e.image}
-                                image2={e.image2}
-                                key={e.id}
-                                name={e.name}
-                                price={e.price.toString()}
-                                id={Number(e.id)}
-                                promotion={e.promotion}
-                                promotional_price={e.promotional_price}
-                                />)}})
-                    }
-                </div>:
-                <p className="font-poppins text-center text-2xl font-medium">No has agregado productos a tus favoritos</p>
+  return (
+    <div>
+      <h3 className="text-3xl font-semibold text-center font-rift">
+        Productos Favoritos
+      </h3>
+      {productsFavorites.length ? (
+        <div className="grid grid-cols-1 mx-auto w-fit md:grid-cols-2 lg:grid-cols-3 md:gap-x-12 xl:grid-cols-4 2xl:grid-cols-6">
+          {productsFavorites.map((e) => {
+            if (e.show_in_shop) {
+              return (
+                <Product
+                  image={e.image}
+                  image2={e.image2}
+                  key={e.id}
+                  name={e.name}
+                  price={e.price.toString()}
+                  id={Number(e.id)}
+                  promotion={e.promotion}
+                  promotional_price={e.promotional_price}
+                />
+              );
             }
+          })}
         </div>
-    )
-}
+      ) : (
+        <p className="mt-8 text-xl font-medium text-center font-poppins">
+          No has agregado productos a tus favoritos
+        </p>
+      )}
+    </div>
+  );
+};
 
-export default Favorites
+export default Favorites;

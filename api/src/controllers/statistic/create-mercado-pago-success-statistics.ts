7069e60fa6f4691b4b72a1b8db33d8d18ffe4c64
Matías Carrizo <matiascarrizo.msjz@gmail.com>
2023-02-22 11:07:05 -0300

    "Información de mercadopago para estadisticas"

--- api/src/controllers/statistic/create-mercado-pago-success-statistics.ts
+++ api/src/controllers/statistic/create-mercado-pago-success-statistics.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
 import db from '../../database/database';
 
-export async function createPaymentSuccessStatistics(id_user: number, id_order: number) {
+export async function createPaymentSuccessStatistics(id_order: number, status: string, transaction_amount: number, shipping_amount: number, total_paid_amount: number) {
     try {
         const paymentStatistic = {
             action_type: "payment_success",
             action_date: new Date(),
-            id_user: id_user,
+            //id_user: id_user,
             id_order: id_order,
         };
 
